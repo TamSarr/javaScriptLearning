8fbb1b3647a32fe93dda069c5b1db0851606e4a3
himic <tamsar1111@gmail.com>
2019-02-28 18:17:11 +0200

    SYS: init

---
+++ dev/js/scripts.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// Exercise 1
+alert("I'm JavaScript");
+alert("Or ECMAScript");
+
+// Exercise 2
+var admin;
+var name = "Василий";
+admin = name;
+alert(admin);
+
+// Exercise 3
+var planetName = "Земля";
+var visitorName = "Петя";
+
+// Exercise 4
+var a = "a";
+var b = 1;
+console.log(a > b);
+
+// Exercise 5
+var age = 19;
+if (age < 14 || age > 90) {
+	alert("true");
+} else {
+	alert("false");
+}
+
+// Exercise 7
+start: for ( var i = 2; i <= 10; i++ ) {
+	for ( var j = 1; j < i; j++ ) {
+		if( i % j == 0 );
+	}
+}
